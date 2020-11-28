bdd5fcba8742fff512f3761c9af9d2e6717e410d
Breadp4ck <iii103@mail.ru>
2020-11-28 11:43:47 +0700

    Add invert operator

--- csharp/pz3/compile.sh
+++ csharp/pz3/compile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 csc /t:exe /out:Program.exe Program.cs BinaryOperation.cs \
-    Function.cs Constant.cs IExpr.cs Variable.cs
+    Function.cs Constant.cs IExpr.cs Variable.cs \
+    UnaryOperation.cs
